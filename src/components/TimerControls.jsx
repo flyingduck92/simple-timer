06a85824db875e3cfa79685fff412f2059f2b8d2
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-09-05 01:15:05 +0700

    useRef forfocus on the button

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useRef } from 'react'
+
 const TimerControls = ({ isRunning, onToggle, onReset }) => {
+  const startBtnRef = useRef(null)
+
+  useEffect(() => {
+    if (startBtnRef.current) {
+      startBtnRef.current.focus()
+    }
+  }, [])
+
   return (
     <>
-      <button onClick={onToggle} className="cursor-pointer mt-3 mr-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+      <button ref={startBtnRef} onClick={onToggle} className="cursor-pointer mt-3 mr-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
         {isRunning ? 'Pause' : 'Start'}
       </button>
       <button onClick={onReset} className="cursor-pointer mt-3 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
